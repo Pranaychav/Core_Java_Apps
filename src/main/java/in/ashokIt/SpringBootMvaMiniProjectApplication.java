13f850c08d86b2b3814ee5d4e265d8ded1c8dcd1
Pranaychav <chavanvipul493@gmail.com>
2024-05-18 20:58:09 +0530

    first commit

---
+++ src/main/java/in/ashokIt/SpringBootMvaMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package in.ashokIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvaMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvaMiniProjectApplication.class, args);
+	}
+
+}
